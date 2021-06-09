41b29498e01a47c71c645b2d96cc0fbe3f6c1a10
LEGENDSSRA10 <74900936+LEGENDSSRA10@users.noreply.github.com>
2021-06-09 10:36:54 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,13 @@
+//class Paper
+//{
+    var options = 
+    {
+        isStatic: false,
+        restitution: 0.3,
+        friction: 0,
+        density: 1.2
+
+        //this.image = loadImage("paper.png")
+    }
+    //this.image = loadImage("paper.png")
+//}
